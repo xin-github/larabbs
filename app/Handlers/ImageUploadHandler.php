ecbe7e56abbf7a4612ddcdf383f85c158421b11d
vagrant <vagrant@homestead>
2019-05-15 04:24:39 +0000

    裁剪头像

--- app/Handlers/ImageUploadHandler.php
+++ app/Handlers/ImageUploadHandler.php
@@ -2,28 +2,52 @@
 
 namespace App\Handlers;
 
+use Image;
+
 class ImageUploadHandler
 {
     protected $allowed_ext = ['jpg', 'png', 'gif', 'jpeg'];
 
-    public function save($file, $folder, $file_prefix)
+    public function save($file, $folder, $file_prefix, $max_width = false)
     {
-        $folder_name = "uploads/images/$folder/".date("Ym/d", time());
+        $folder_name = "uploads/images/$folder/" . date("Ym/d", time());
 
-        $upload_path = public_path().'/'.$folder_name;
+        $upload_path = public_path() . '/' . $folder_name;
 
-        $extension = strtolower($file->getClientOriginalExtension()) ? : 'png';
+        $extension = strtolower($file->getClientOriginalExtension()) ?: 'png';
 
-        $file_name = $file_prefix.'_'.time().'_'.str_random(10).'.'.$extension;
+        $file_name = $file_prefix . '_' . time() . '_' . str_random(10) . '.' . $extension;
 
-        if (! in_array($extension, $this->allowed_ext)) {
+        if (!in_array($extension, $this->allowed_ext)) {
             return false;
         }
 
         $file->move($upload_path, $file_name);
 
+        if ($max_width && $extension != 'gif') {
+
+            $this->reduceSize($upload_path . '/' . $file_name, $max_width);
+        }
+
         return [
-            'path' => config('app.url')."/$folder_name/$file_name"
+            'path' => config('app.url') . "/$folder_name/$file_name"
         ];
+    }
+
+    public function reduceSize($file_path, $max_width)
+    {
+        $image = Image::make($file_path);
+
+        $image->resize($max_width, null, function ($constraint) {
+
+            // 设定宽度是 $max_width，高度等比例双方缩放
+            $constraint->aspectRatio();
+
+            // 防止裁图时图片尺寸变大
+            $constraint->upsize();
+        });
+
+        // 对图片修改后进行保存
+        $image->save();
     }
 }
