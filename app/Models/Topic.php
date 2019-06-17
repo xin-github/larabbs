5b21b451d3b318f131455f4e3c3aeb871562cebc
vagrant <vagrant@homestead>
2019-06-17 01:58:19 +0000

    话题排序

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -9,11 +9,37 @@ class Topic extends Model
         'view_count', 'last_reply_user_id',
         'order', 'excerpt', 'slug'];
 
-    public function category(){
+    public function category()
+    {
         return $this->belongsTo(Category::class);
     }
 
-    public function user(){
+    public function user()
+    {
         return $this->belongsTo(User::class);
+    }
+
+    public function scopeWithOrder($query, $order)
+    {
+        switch ($order) {
+            case 'recent':
+                $query->recent();
+                break;
+
+            default:
+                $query->recentReplied();
+                break;
+        }
+
+        return $query->with('user', 'category');
+    }
+
+    public function scopeRecentReplied($query)
+    {
+        return $query->orderBy('updated_at', 'desc');
+    }
+
+    public function scopeRecent($query){
+        return $query->orderBy('created_at', 'desc');
     }
 }
